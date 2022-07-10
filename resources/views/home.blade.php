7eb69a0feabcb849596c79b7d8dae2a8f4f8afaa
Antonio Tomas <antoniotd87@gmail.com>
2022-07-10 13:48:48 -0500

    Creacion del componente

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="d-flex justify-content-between">
+        <h4 class="card-title"><b>Component Title </b> | Page Title</h4>
+        <div>
+            <div class="btn btn-info ml-2">Agregar</div>
         </div>
     </div>
-</div>
+    <input type="text" class="form-control" name="search" id="search" placeholder="Buscar">
+    <hr>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="rounded">
+        <table class="table">
+            <thead class="thead-light" style="border-radius: 15px">
+                <tr>
+                    <th>#</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
